b932831438135ca496989512ebb52523e8afaf1a
Rebecca <rhdai10@gmail.com>
2020-12-02 18:10:48 -0800

    aoc day2 part2

--- day2/day2.js
+++ day2/day2.js
@@ -2,20 +2,18 @@ const readFile = require('../helper.js');
 
 const input = readFile().split('\n');
 
-function part1() {
-  const validPasswords = input.filter((password) => validatePassword(password));
+function generateCaptureGroups(password) {
+  const regex = /(?<lowerBound>\d+)-(?<upperBound>\d+)\s+(?<policyLetter>[a-z]):\s+(?<password>.*\w)/;
 
-  return validPasswords.length;
+  return password.match(regex).groups;
 }
 
-function validatePassword(password) {
-  const regex = /(?<lowerBound>\d+)-(?<upperBound>\d+)\s+(?<policy>[a-z]):\s+(?<password>.*\w)/;
-
-  const captureGroups = password.match(regex).groups;
+function validatePasswordByOccurance(password) {
+  const captureGroups = generateCaptureGroups(password);
 
   const actualOccurance = captureGroups.password
     .split('')
-    .filter((char) => char === captureGroups.policy).length;
+    .filter((char) => char === captureGroups.policyLetter).length;
 
   return (
     captureGroups.lowerBound <= actualOccurance &&
@@ -23,4 +21,27 @@ function validatePassword(password) {
   );
 }
 
-console.log('Part 1: ', part1());
+function validatePasswordByPosition(password) {
+  const captureGroups = generateCaptureGroups(password);
+
+  const firstPosition = captureGroups.password[captureGroups.lowerBound - 1];
+  const secondPosition = captureGroups.password[captureGroups.upperBound - 1];
+
+  return (
+    firstPosition !== secondPosition &&
+    (firstPosition === captureGroups.policyLetter ||
+      secondPosition === captureGroups.policyLetter)
+  );
+}
+
+function part1() {
+  return input.filter((password) => validatePasswordByOccurance(password))
+    .length;
+}
+
+function part2() {
+  return input.filter((password) => validatePasswordByPosition(password))
+    .length;
+}
+
+console.log('Part 1: ', part1(), 'Part 2: ', part2());
