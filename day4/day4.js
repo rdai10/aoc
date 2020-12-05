f7d9f2a0ee077a5a5241e2cf4160a7cb5f39fd13
Rebecca <rhdai10@gmail.com>
2020-12-04 18:34:12 -0800

    aoc day4 part2

--- day4/day4.js
+++ day4/day4.js
@@ -1,5 +1,14 @@
 const readFile = require('../helper.js');
 
+const BIRTH_YEAR_FIELD = 'byr';
+const COUNTRY_ID_FIELD = 'cid';
+const EXPIRATION_YR_FIELD = 'eyr';
+const EYE_COLOR_FIELD = 'ecl';
+const HAIR_COLOR_FIELD = 'hcl';
+const HEIGHT_FIELD = 'hgt';
+const ISSUE_YEAR_FIELD = 'iyr';
+const PASSPORT_ID_FIELD = 'pid';
+
 const input = readFile()
   .replace(/\n{1}/g, ' ')
   .split(/\s{2,}/)
@@ -16,17 +25,75 @@ function processPassport(matrix) {
     passport[key] = value;
   }
 
-  passport['fieldLength'] = matrix.length;
-
   return passport;
 }
 
-function part1() {
+function getPassportsWithCompleteInformation() {
   return input.filter(
     (passport) =>
-      passport.fieldLength === 8 ||
-      (passport.fieldLength === 7 && !passport.cid)
-  ).length;
+      passport[BIRTH_YEAR_FIELD] &&
+      passport[EXPIRATION_YR_FIELD] &&
+      passport[EYE_COLOR_FIELD] &&
+      passport[HAIR_COLOR_FIELD] &&
+      passport[HEIGHT_FIELD] &&
+      passport[ISSUE_YEAR_FIELD] &&
+      passport[PASSPORT_ID_FIELD]
+  );
 }
 
-console.log('Part 1: ', part1());
+function validateFieldValue(name, value) {
+  const intValue = parseInt(value);
+
+  switch (name) {
+    case BIRTH_YEAR_FIELD:
+      return 1920 <= intValue && intValue <= 2002;
+    case COUNTRY_ID_FIELD: 
+      return true;
+    case EXPIRATION_YR_FIELD:
+      return 2020 <= intValue && intValue <= 2030;
+    case EYE_COLOR_FIELD:
+      return /^(amb|blu|brn|gry|grn|hzl|oth$)/.test(value);
+    case HAIR_COLOR_FIELD:
+      return /^#[a-f\d]{6}$/.test(value);
+    case HEIGHT_FIELD:
+      if (value.endsWith('cm')) {
+        const heightInCM = parseInt(value.replace('cm', ''));
+
+        return 150 <= heightInCM && heightInCM <= 193;
+      } else if (value.endsWith('in')) {
+        const heightInIN = parseInt(value.replace('in', ''));
+
+        return 59 <= heightInIN && heightInIN <= 76;
+      } else {
+        return false;
+      }
+    case ISSUE_YEAR_FIELD:
+      return 2010 <= intValue && intValue <= 2020;
+    case PASSPORT_ID_FIELD:
+      return /^\d{9}$/.test(value);
+    default:
+      return false;
+  }
+}
+
+function part1() {
+  return getPassportsWithCompleteInformation().length;
+}
+
+function part2() {
+  const passportsWithCompleteInformation = getPassportsWithCompleteInformation();
+
+  const validationResults = passportsWithCompleteInformation.map((entry) => {
+    const fields = Object.entries(entry);
+
+    return fields.every((field) => {
+      const [key, value] = field;
+
+      return validateFieldValue(key, value);
+    });
+  });
+
+  return validationResults.filter((result) => result === true).length;
+}
+
+console.log('Part 1: ', part1(), 'Part 2: ', part2());
