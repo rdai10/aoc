7cc9a21c0b32131b4f978a2779bb6af7cb86ab84
Rebecca <rhdai10@gmail.com>
2021-12-01 09:31:42 -0800

    complete day1

--- 2021/day1/day1.js
+++ 2021/day1/day1.js
@@ -2,11 +2,11 @@ const readFile = require('../../helper');
 
 const report = readFile().split('\n').map(Number);
 
-function part1() {
+function part1(input) {
 	let count = 0;
 
-	report.forEach((scan, index) => {
-		if (index > 0 && scan > report[index - 1]) {
+	input.forEach((scan, index) => {
+		if (index > 0 && scan > input[index - 1]) {
 			count++;
 		}
 	});
@@ -14,4 +14,23 @@ function part1() {
 	return count;
 }
 
-console.log('Part 1: ', part1());
+function part2(input) {
+	const slidingWindow = input
+		.map((_, index) => {
+			if (index < input.length - (input.length % 3)) {
+				return input.slice(index, index + 3);
+			}
+
+			return [];
+		})
+		.filter((item) => item.length === 3)
+		.map((item) => {
+			const [first, second, third] = item;
+
+			return first + second + third;
+		});
+
+	return part1(slidingWindow);
+}
+
+console.log('Part 1: ', part1(report), 'Part 2: ', part2(report));
