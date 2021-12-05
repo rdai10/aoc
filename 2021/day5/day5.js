aace5f17ad97db14139c1453d6a1179bd1963607
Rebecca <rhdai10@gmail.com>
2021-12-05 00:23:14 -0800

    day5 part2

--- 2021/day5/day5.js
+++ 2021/day5/day5.js
@@ -1,5 +1,7 @@
 const readFile = require('../../helper');
 
+const _ = require('lodash');
+
 const vents = readFile()
 	.split(/\n/)
 	.map((line) =>
@@ -7,6 +9,29 @@ const vents = readFile()
 			.split(' -> ')
 			.map((coordinate) => coordinate.split(',').map(Number))
 	);
+
+const horizontalOrVerticalVents = vents.filter(
+	([[startX, startY], [endX, endY]]) => startX === endX || startY === endY
+);
+
+function calculateAllPointsFromDiagonal(line) {
+	const [[startX, startY], [endX, endY]] = line;
+	const dx = endX - startX;
+	const dy = endY - startY;
+
+	let points = [[endX, endY]];
+	let pointX = startX;
+	let pointY = startY;
+
+	while (pointX !== endX && pointY !== endY) {
+		points.push([pointX, pointY]);
+
+		pointX = dx < 0 ? pointX - 1 : pointX + 1;
+		pointY = dy < 0 ? pointY - 1 : pointY + 1;
+	}
+
+	return points;
+}
 
 function calculateAllPointsFromLine(line) {
 	const [[startX, startY], [endX, endY]] = line;
@@ -44,11 +69,9 @@ function calculateAllPointsFromLine(line) {
 	return points;
 }
 
-function trackPoints(vents) {
-	const points = new Map();
-
+function trackPoints(vents, pointsFn, points = new Map()) {
 	const addPointToPoints = (line) => {
-		calculateAllPointsFromLine(line).forEach((point) => {
+		pointsFn(line).forEach((point) => {
 			const pointCoordinates = point.join(',');
 
 			if (points.has(pointCoordinates)) {
@@ -66,14 +89,8 @@ function trackPoints(vents) {
 	return points;
 }
 
-function part1() {
+function countOverlap(pointsTracked) {
 	let counter = 0;
-
-	const horizontalOrVerticalVents = vents.filter(
-		([[startX, startY], [endX, endY]]) => startX === endX || startY === endY
-	);
-
-	const pointsTracked = trackPoints(horizontalOrVerticalVents);
 
 	for (const point of pointsTracked) {
 		const [_, count] = point;
@@ -86,4 +103,30 @@ function part1() {
 	return counter;
 }
 
-console.log('Part 1: ', part1());
+function part1() {
+	const points = trackPoints(
+		horizontalOrVerticalVents,
+		calculateAllPointsFromLine
+	);
+
+	return countOverlap(points);
+}
+
+function part2() {
+	const diagonalVents = _.difference(vents, horizontalOrVerticalVents);
+
+	const horizontalAndVertialPointsTracked = trackPoints(
+		horizontalOrVerticalVents,
+		calculateAllPointsFromLine
+	);
+
+	const points = trackPoints(
+		diagonalVents,
+		calculateAllPointsFromDiagonal,
+		horizontalAndVertialPointsTracked
+	);
+
+	return countOverlap(points);
+}
+
+console.log('Part 1: ', part1(), 'Part 2: ', part2());
